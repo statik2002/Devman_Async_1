085fc8914b3da9e1a84905bf24147a88149ed98e
statik2002 <statik2002@mail.ru>
2022-09-25 13:00:26 +0800

    12 шаг

--- main.py
+++ main.py
@@ -1,59 +1,108 @@
 import asyncio
+import random
 import time
 import curses
 from curses import wrapper
+from itertools import cycle
+
+from curses_tools import draw_frame, load_sprite, read_controls
 
 
 async def blink(canvas, row, column, symbol='*'):
-    TIC_TIMEOUT = 0.1
+
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(0, 20):
+        for _ in range(random.randint(1, 20)):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(0, 3):
+        for _ in range(3):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(0, 5):
+        for _ in range(5):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(0, 3):
+        for _ in range(3):
             await asyncio.sleep(0)
 
 
-def draw_star(row, column, canvas):
-    canvas.addch(row, column, '*', curses.A_DIM)
-    canvas.refresh()
-    time.sleep(2)
-    canvas.addch(row, column, '*')
-    canvas.refresh()
-    time.sleep(0.3)
-    canvas.addch(row, column, '*', curses.A_BOLD)
-    time.sleep(0.5)
-    canvas.refresh()
-    time.sleep(0.5)
-    canvas.addch(row, column, '*')
-    canvas.refresh()
-    time.sleep(0.3)
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
+async def animate_spaceship(canvas, start_row, start_column, sprites, negative=False):
+    for sprite in cycle(sprites):
+        draw_frame(canvas, start_row, start_column, sprite, negative=negative)
+        for _ in range(2):
+            await asyncio.sleep(0)
+        draw_frame(canvas, start_row, start_column, sprite, negative=True)
 
 
 def draw(canvas):
     curses.curs_set(0)
     canvas.border()
-    count = 5
-    coroutine = blink(canvas, 5, 5)
-    while count > 0:
+    canvas.nodelay(True)
+    max_row, max_col = canvas.getmaxyx()
 
-        try:
-            coroutine.send(None)
-            canvas.refresh()
-            time.sleep(1)
-        except StopIteration:
-            coroutine = blink(canvas, 5, 5)
-            count -= 1
+    spaceship_sprite1 = load_sprite('sprites/rocket_frame_1.txt')
+    spaceship_sprite2 = load_sprite('sprites/rocket_frame_2.txt')
+    TIC_TIMEOUT = 0.1
+    SPACESHIP_ROW_POSITON = max_row//2
+    SPACESHIP_COL_POSITON = max_col//2
+
+    type_of_stars = '+*.:`"'
+
+    coroutines = [blink(canvas, random.randint(2, max_row-2), random.randint(2, max_col-2), symbol=random.choice(type_of_stars)) for i in range(0, 200)]
+    coroutine_fire = fire(canvas, max_row//2, max_col//2)
+    coroutines.append(coroutine_fire)
+    coroutines.append(animate_spaceship(canvas, SPACESHIP_ROW_POSITON, SPACESHIP_COL_POSITON, [spaceship_sprite1, spaceship_sprite2]))
+    #coroutines_spaceship = animate_spaceship(canvas, SPACESHIP_ROW_POSITON, SPACESHIP_COL_POSITON, [spaceship_sprite1, spaceship_sprite2])
+
+    while True:
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+                row_direction, col_direction, space_pressed = read_controls(canvas)
+                SPACESHIP_ROW_POSITON += row_direction
+                SPACESHIP_COL_POSITON += col_direction
+
+            except StopIteration:
+                coroutines.remove(coroutine)
+
+        if len(coroutines) == 0:
+            coroutines = [blink(canvas, random.randint(2, max_row-2), random.randint(2, max_col-2), symbol=random.choice(type_of_stars)) for i in range(0, 200)]
+            coroutines.append(animate_spaceship(canvas, SPACESHIP_ROW_POSITON, SPACESHIP_COL_POSITON, [spaceship_sprite1, spaceship_sprite2]))
+
+        canvas.refresh()
+        time.sleep(TIC_TIMEOUT)
 
 
 if __name__ == '__main__':
